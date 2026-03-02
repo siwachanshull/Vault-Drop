7f2b17db18577347376a80dfbde08d6d12f8a3d8
Anshul Siwach <anshulsiwach07@gmail.com>
2026-03-02 21:54:39 +0530

    deciding on new way to encrypt file

--- DropVault Frontend/frontend0fileshare/src/pages/Subscriptions.jsx
+++ DropVault Frontend/frontend0fileshare/src/pages/Subscriptions.jsx
@@ -79,7 +79,7 @@ fetchUserCredits();
                 <div className={`mb-4 p-3 rounded ${messageType === "error" ? "bg-red-100 text-red-700" : "bg-green-100 text-green-700"}`}>
                   {message} 
                   <div>
-                     <p>not able to do changes because of exams</p>
+                     <p>leetcode todays ques done</p>
                     </div>
             </div>
         </DashboardLayout>
